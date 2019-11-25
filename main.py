35b8326e10708aa8d7f6bf71b94e424fee0db850
Rubix327 <yarik.cool1@mail.ru>
2019-11-25 23:26:51 +0300

    Started working on backup saver

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+sources = []
+
+# Entering paths where to copy files from
+
+active = True
+while active:
+    queue = input('Enter the path you will copy the files from.\n(or \'q\' to exit queue): ')
+    if queue == 'q':
+        active = False
+    else:
+        sources.append(queue)
+
+mainCatalogPath = 'C:\\' ############# Изменить путь главного каталога!
+
+# Changing the main reservation catalog path
+
+changedPath = input('Enter new path of main reservation catalog:\n(or \'q\' if you don\'t want to): ')
+if changedPath == 'q':
+    pass
+else:
+    mainCatalogPath = changedPath
+
+#
